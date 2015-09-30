95a797641f66675bc8c34fdcab1516fe4702a9a7
Leonardo Rifeli <leonardorifeli@gmail.com>
2015-09-30 00:07:41 -0300

    Created the calculator using if

---
+++ school/2015-09-29/calculator-if.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int numberPrimary;
+    int numberSecondary;
+    int option;
+    float result;
+
+    printf("\n---------------------------------\n");
+    printf("Please, choose an option of operation: \n");
+    printf("* 1 = Addition \n");
+    printf("* 2 = Subtraction \n");
+    printf("* 3 = Multiplication \n");
+    printf("* 4 = Division \n");
+    printf("* 5 = MOD \n");
+    printf("* 6 = Exit \n");
+    scanf("%d", &option);
+
+    if(option == 6){
+        printf("-----BYE----- \n");
+        exit(0);
+    }
+
+    printf("Please, insert the number primary: ");
+    scanf("%d", &numberPrimary);
+
+    printf("\nPlease, insert the number secondary: ");
+    scanf("%d", &numberSecondary);
+
+    if(option == 1){
+        result = ((numberPrimary)+(numberSecondary));
+        printf("Result addition is: %.2f \n", result);
+    }else if(option == 2){
+        result = ((numberPrimary)-(numberSecondary));
+        printf("Result subtraction is: %.2f \n", result);
+    }else if(option == 3){
+        result = ((numberPrimary)*(numberSecondary));
+        printf("Result multiplication is: %.2f \n", result);
+    }else if(option == 4){
+        result = ((numberPrimary)/(numberSecondary));
+        printf("Result division is: %.2f \n", result);
+    }else if(option == 5){
+        result = (numberPrimary % numberSecondary);
+        printf("Result the mod is: %.2f \n", result);
+    }else{
+        printf("-----BYE----- \n");
+        exit(0);
+    }
+
+    printf("\n---------------------------------\n");
+
+}
