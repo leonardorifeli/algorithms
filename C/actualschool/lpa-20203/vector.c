dca97bf96adde1c09ca002e357c464c52c037e77
Leonardo Rifeli <leonardorifeli@gmail.com>
2020-12-10 20:59:49 -0300

    fixed number

--- C/actualschool/lpa-20203/vector.c
+++ C/actualschool/lpa-20203/vector.c
@@ -8,16 +8,20 @@ void sort(int numbers[4], int totalNumbers);
 int main() {
     // Set Portuguese with locale
     setlocale(LC_ALL, "Portuguese");
-    
+
+    // Set default numbers vector and other variables
     int numbers[4], i, totalNumbers = 5;
 
+    // Loop to get all 5 integer numbers
     for(i=0;i < totalNumbers; i++) {
         printf("Please, insert %d number:\n", i + 1);
         scanf("%d", &numbers[i]);
     }
 
+    // Sort all numbers by int ASC
     sort(numbers, totalNumbers);
 
+    // Show sorted numbers
     printf("\n\n** Thanks! Ordered number list is:\n");
     for(i=0;i < totalNumbers; i++) {
         printf("# %d\n", numbers[i]);
@@ -31,7 +35,7 @@ void sort(int numbers[4], int totalNumbers) {
     // Data to use on for
     int i, j, temporaryNumber;
 
-    // Parse stores and order price ASC
+    // Parse numbers and order integer ASC
     for (i = 0; i < totalNumbers - 1; i++) {
         for (j = 0; j < (totalNumbers - 1-i); j++) {
             if (numbers[j] > numbers[j + 1]) {
