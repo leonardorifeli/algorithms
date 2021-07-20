8273dd7ef6b527fd7d414a7c7bc4b9410dd0cb31
Leonardo Rifeli <leonardorifeli@gmail.com>
2021-07-19 22:47:01 -0300

    Fix comments

--- C/actualschool/estruturasdedados-202106/exec1.c
+++ C/actualschool/estruturasdedados-202106/exec1.c
@@ -10,7 +10,7 @@ struct Music {
   struct Music *next;
 } *Data;
 
-
+// Define all functions
 int displayMenu();
 void addMusic();
 void listMusic();
@@ -18,33 +18,39 @@ void listMusic();
 int main() {
   setlocale(LC_ALL, "Portuguese");
 
+  // Setup menu vars
   int menuOption, clean;
   Data = NULL;
 
+  // Creating while loop to work with option menu.
   while(1) {
+    // Call menu function
     menuOption = displayMenu();
 
+    // Validating invalid option menu
     if(menuOption < 1 || menuOption > 3) {
       printf("Invalid Option.\n\n");
       continue;
     }
 
+    // Check option is to exit
     if(menuOption == 3) {
       printf("Thanks! Bye.\n");
       break;
     }
 
+    // Check option is to list musics
     if(menuOption == 2) {
       listMusic();
       continue;
     }
 
+    // Call func to add music
     addMusic();
   }
-  
-
 }
 
+// Fuction to display menu option
 int displayMenu() {
   int menuOption, clean;
   
@@ -55,20 +61,26 @@ int displayMenu() {
   printf("[ 3 ] Exit \n");
   printf("***************************** \n");
 
+  // Read menu choice
   scanf("%d", &menuOption);
+
+  // Clean keyboard
   while ((clean = getchar()) != '\n' && clean != EOF) {}
 
   return menuOption;
 }
 
+// Function to add music
 void addMusic() {
   int clean;
   
+  // Create a new music
   struct Music *newMusic;
 	newMusic = (struct Music *)malloc(sizeof(struct Music));
 	
   printf("Inserting new music \n");
   
+  // Get all music data
   printf("- Music name: \n");
   fgets(newMusic->name, 20, stdin);
   
@@ -79,28 +91,34 @@ void addMusic() {
   scanf("%f", &newMusic->duration);
   while ((clean = getchar()) != '\n' && clean != EOF) {}
 
+  // Check music is empty
 	if (Data == NULL) {
     Data = newMusic;
 		Data->next = NULL;
     return;
 	}
 
+  // Music already exits and adding on next music
 	newMusic->next = Data;
   Data = newMusic;
 }
 
+// Function to show music
 void listMusic() {
+  // Create a music list with data
   struct Music *musicList;
 	musicList = (struct Music *)malloc(sizeof(struct Music));
   musicList = Data;
 
   printf("Listing musics \n");
 
+  // Checking music list is empty
   if (Data == NULL) {
     printf("The music list is empty.\n");
     return;
   }
 
+  // Loop to show all music data
   while (musicList != NULL) {
 		printf("- Music name: %s", musicList->name);
     printf("- Artist name: %s", musicList->artist);
@@ -109,13 +127,3 @@ void listMusic() {
     printf("***************************** \n");
 	}
 }
-
-// Função para montar um produto com suas propriedades
-// struct Product build(int item, char name[20], int code, float price) {
-//   struct Product product;
-//   product.item = item;
-//   product.name = name;
-//   product.code = code;
-//   product.price = price;
-//   return product;
-// }
